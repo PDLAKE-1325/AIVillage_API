96c11be6b2e0eeb20fe890dcb827618012fe2381
PDLAKE-1325 <163096820+PDLAKE-1325@users.noreply.github.com>
2026-03-16 10:12:05 +0900

    dd

--- cowcat_village/cowcat_village/settings.py
+++ cowcat_village/cowcat_village/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--i9hh)hqn1)+v)xv7oqxi0=5i7zy-c)k2s4s!6w88xitg^ip=f'
+# SECRET_KEY = 'django-insecure--i9hh)hqn1)+v)xv7oqxi0=5i7zy-c)k2s4s!6w88xitg^ip=f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
